aaf07156f029135b46ff1ddf9ddc44d2209d9354
Adam Morgan <amorgan@scottlogic.co.uk>
2018-10-08 15:19:55 +0100

    Updates to moving explorer step

--- script.js
+++ script.js
@@ -2,23 +2,170 @@ let type = "WebGL"
 if(!PIXI.utils.isWebGLSupported()){
   type = "canvas"
 }
-
 PIXI.utils.sayHello(type)
 
-//Create a Pixi Application
-let app = new PIXI.Application({
-  width: 256,         // default: 800
-  height: 256,        // default: 600
-  antialias: true,    // default: false
-  transparent: false, // default: false
-  resolution: 1       // default: 1
-});
+//Aliases
+let Application = PIXI.Application,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    Sprite = PIXI.Sprite,
+    TextureCache = PIXI.utils.TextureCache;
 
-app.renderer.backgroundColor = 0x061639;
-app.renderer.view.style.position = "absolute";
-app.renderer.view.style.display = "block";
-app.renderer.autoResize = true;
-app.renderer.resize(window.innerWidth, window.innerHeight);
+//Create a Pixi Application
+let app = new Application({
+    width: 512,
+    height: 512,
+    antialias: true,
+    transparent: false,
+    resolution: 1
+});
 
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
+
+loader
+  .add([
+    "images/cat.png",
+    "images/tileset.png",
+    "images/treasureHunter.json"
+  ])
+  .on("progress", loadProgressHandler)
+  .load(setup_treasure);
+
+function loadProgressHandler(loader, resource) {
+
+  //Display the file `url` currently being loaded
+  console.log("loading: " + resource.url);
+
+  //Display the percentage of files currently loaded
+  console.log("progress: " + loader.progress + "%");
+
+  //If you gave your files names as the first argument
+  //of the `add` method, you can access them like this
+  //console.log("loading: " + resource.name);
+}
+
+function setup_cat() {
+  console.log("setup");
+  let cat = new Sprite(
+    resources["images/cat.png"].texture
+  );
+
+  cat.x = 96;
+  cat.y = 96;
+  cat.anchor.x = 0.5;
+  cat.anchor.y = 0.5;
+  cat.rotation = 0.5;
+
+  app.stage.addChild(cat);
+}
+
+function setup_rocket() {
+  console.log("setup");
+  //Create the `tileset` sprite from the texture
+  let texture = TextureCache["images/tileset.png"];
+  //Create a rectangle object that defines the position and
+  //size of the sub-image you want to extract from the texture
+  //(`Rectangle` is an alias for `PIXI.Rectangle`)
+  let rectangle = new PIXI.Rectangle(192, 128, 64, 64);
+  //Tell the texture to use that rectangular section
+  texture.frame = rectangle;
+
+  //Create the sprite from the texture
+  let rocket = new Sprite(texture);
+  //Position the rocket sprite on the canvas
+  rocket.x = 32;
+  rocket.y = 32;
+
+  //Add the rocket to the stage
+  app.stage.addChild(rocket);
+}
+
+//Define variables that might be used in more
+//than one function
+let dungeon, explorer, treasure, door, textures;
+
+function setup_treasure() {
+  //One way of using gameLoop
+  //app.ticker.add(delta => gameLoop(delta));
+
+  //There are 3 ways to make sprites from textures atlas frames
+
+  //1. Access the `TextureCache` directly
+  let dungeonTexture = TextureCache["dungeon.png"];
+  dungeon = new Sprite(dungeonTexture);
+  app.stage.addChild(dungeon);
+
+  //2. Access the texture using through the loader's `resources`:
+  explorer = new Sprite(
+    resources["images/treasureHunter.json"].textures["explorer.png"]
+  );
+  explorer.x = 68;
+
+  //Center the explorer vertically
+  explorer.y = app.stage.height / 2 - explorer.height / 2;
+  app.stage.addChild(explorer);
+
+  //3. Create an optional alias called `textures` for all the texture atlas
+  //frame id textures.
+  textures = PIXI.loader.resources["images/treasureHunter.json"].textures;
+
+  //Make the treasure box using the alias
+  treasure = new Sprite(textures["treasure.png"]);
+  app.stage.addChild(treasure);
+
+  //Position the treasure next to the right edge of the canvas
+  treasure.x = app.stage.width - treasure.width - 48;
+  treasure.y = app.stage.height / 2 - treasure.height / 2;
+  app.stage.addChild(treasure);
+
+  //Make the exit door
+  door = new Sprite(textures["door.png"]);
+  door.position.set(32, 0);
+  app.stage.addChild(door);
+
+  //Make the blobs
+  let numberOfBlobs = 6,
+      spacing = 48,
+      xOffset = 150;
+
+  //Make as many blobs as there are `numberOfBlobs`
+  for (let i = 0; i < numberOfBlobs; i++) {
+
+    //Make a blob
+    let blob = new Sprite(textures["blob.png"]);
+
+    //Space each blob horizontally according to the `spacing` value.
+    //`xOffset` determines the point from the left of the screen
+    //at which the first blob should be added.
+    let x = spacing * i + xOffset;
+
+    //Give the blob a random y position
+    //(`randomInt` is a custom function - see below)
+    let y = randomInt(0, app.stage.height - blob.height);
+
+    //Set the blob's position
+    blob.x = x;
+    blob.y = y;
+
+    //Add the blob sprite to the stage
+    app.stage.addChild(blob);
+  }
+}
+
+function gameLoop(delta){
+  requestAnimationFrame(gameLoop);
+  //Move the explorer 1 pixel
+  if (explorer) {
+    explorer.vx = 1;
+    explorer.x += explorer.vx;
+  }
+}
+
+//Start the loop
+gameLoop();
+
+//The `randomInt` helper function
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
