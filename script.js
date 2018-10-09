57342cedc40ded3ff4c515fd798ad3bfe413ce89
Adam Morgan <amorgan@scottlogic.co.uk>
2018-10-09 14:05:57 +0100

    Tidy up script and remove unused images

--- script.js
+++ script.js
@@ -20,105 +20,86 @@ let app = new Application({
     resolution: 1
 });
 
-//Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
 
 loader
   .add([
-    "images/cat.png",
-    "images/tileset.png",
     "images/treasureHunter.json"
   ])
   .on("progress", loadProgressHandler)
   .load(setup);
 
 function loadProgressHandler(loader, resource) {
-
-  //Display the file `url` currently being loaded
   console.log("loading: " + resource.url);
-
-  //Display the percentage of files currently loaded
   console.log("progress: " + loader.progress + "%");
-
-  //If you gave your files names as the first argument
-  //of the `add` method, you can access them like this
-  //console.log("loading: " + resource.name);
 }
 
-//Define variables that might be used in more
-//than one function
-let dungeon, explorer, treasure, door, textures, state;
+let state, explorer, treasure, blobs, chimes, exit, player, dungeon,
+    door, healthBar, message, gameScene, gameOverScene, enemies,  textures;
 
 function setup() {
-  //One way of using gameLoop
-  //app.ticker.add(delta => gameLoop(delta));
+  setupSprites();
+  setup_keyboard_controls();
+  startGame();
+}
 
-  //There are 3 ways to make sprites from textures atlas frames
+function setupSprites() {
+  textures = PIXI.loader.resources["images/treasureHunter.json"].textures;
 
-  //1. Access the `TextureCache` directly
-  let dungeonTexture = TextureCache["dungeon.png"];
-  dungeon = new Sprite(dungeonTexture);
+  dungeon = new Sprite(textures["dungeon.png"]);
   app.stage.addChild(dungeon);
 
-  //2. Access the texture using through the loader's `resources`:
-  explorer = new Sprite(
-    resources["images/treasureHunter.json"].textures["explorer.png"]
-  );
+  explorer = new Sprite(textures["explorer.png"]);
   explorer.x = 68;
-
-  //Center the explorer vertically
   explorer.y = app.stage.height / 2 - explorer.height / 2;
   explorer.vx = 0;
   explorer.vy = 0;
-  
   app.stage.addChild(explorer);
 
-  //3. Create an optional alias called `textures` for all the texture atlas
-  //frame id textures.
-  textures = PIXI.loader.resources["images/treasureHunter.json"].textures;
-
-  //Make the treasure box using the alias
   treasure = new Sprite(textures["treasure.png"]);
-  app.stage.addChild(treasure);
-
-  //Position the treasure next to the right edge of the canvas
   treasure.x = app.stage.width - treasure.width - 48;
   treasure.y = app.stage.height / 2 - treasure.height / 2;
   app.stage.addChild(treasure);
 
-  //Make the exit door
   door = new Sprite(textures["door.png"]);
   door.position.set(32, 0);
   app.stage.addChild(door);
 
-  //Make the blobs
   let numberOfBlobs = 6,
       spacing = 48,
       xOffset = 150;
 
-  //Make as many blobs as there are `numberOfBlobs`
   for (let i = 0; i < numberOfBlobs; i++) {
-
-    //Make a blob
     let blob = new Sprite(textures["blob.png"]);
-
     //Space each blob horizontally according to the `spacing` value.
     //`xOffset` determines the point from the left of the screen
     //at which the first blob should be added.
     let x = spacing * i + xOffset;
-
     //Give the blob a random y position
     //(`randomInt` is a custom function - see below)
     let y = randomInt(0, app.stage.height - blob.height);
 
-    //Set the blob's position
     blob.x = x;
     blob.y = y;
-
-    //Add the blob sprite to the stage
     app.stage.addChild(blob);
   }
+}
 
+function startGame() {
+  //Set the game state
+  state = play;
+  //start game loop
+  app.ticker.add(delta => gameLoop(delta));
+}
+
+function gameLoop(delta){
+  //requestAnimationFrame(gameLoop);
+  if(state) {
+    state(delta)
+  }
+}
+
+function setup_keyboard_controls() {
   //Capture the keyboard arrow keys
   let left = keyboard(37),
       up = keyboard(38),
@@ -174,16 +155,6 @@ function setup() {
       explorer.vy = 0;
     }
   };
-
-  //Set the game state
-  state = play;
-}
-
-function gameLoop(delta){
-  requestAnimationFrame(gameLoop);
-  if(state) {
-    state(delta)
-  }
 }
 
 function play(delta) {
@@ -193,13 +164,48 @@ function play(delta) {
   }
 }
 
-//Start the loop
-gameLoop();
-
 //The `randomInt` helper function
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+//The `hitTestRectangle` function
+function hitTestRectangle(r1, r2) {
+
+  //Define the variables we'll need to calculate
+  let hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
+
+  //hit will determine whether there's a collision
+  hit = false;
+
+  //Find the center points of each sprite
+  r1.centerX = r1.x + r1.width / 2;
+  r1.centerY = r1.y + r1.height / 2;
+  r2.centerX = r2.x + r2.width / 2;
+  r2.centerY = r2.y + r2.height / 2;
+
+  //Find the half-widths and half-heights of each sprite
+  r1.halfWidth = r1.width / 2;
+  r1.halfHeight = r1.height / 2;
+  r2.halfWidth = r2.width / 2;
+  r2.halfHeight = r2.height / 2;
+
+  //Calculate the distance vector between the sprites
+  vx = r1.centerX - r2.centerX;
+  vy = r1.centerY - r2.centerY;
+
+  //Figure out the combined half-widths and half-heights
+  combinedHalfWidths = r1.halfWidth + r2.halfWidth;
+  combinedHalfHeights = r1.halfHeight + r2.halfHeight;
+
+  if (Math.abs(vx) < combinedHalfWidths && Math.abs(vy) < combinedHalfHeights) {
+    hit = true;
+  } else {
+    hit = false;
+  }
+
+  return hit;
+};
 
 function keyboard(keyCode) {
   let key = {};
